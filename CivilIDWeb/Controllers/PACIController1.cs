f5c93e6c8a3c7dfe78373fd3c56142096f93065c
Kirankumar Reddy <kirannallandula@gmail.com>
2022-09-12 20:56:38 +0530

    logs added for the /getperson and /callback api

--- CivilIDWeb/Controllers/PACIController1.cs
+++ CivilIDWeb/Controllers/PACIController1.cs
@@ -8,6 +8,7 @@ using PACI.MobileId.IntegrationServices.Client;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 
 namespace CivilIDWeb.Controllers
 {
@@ -35,17 +36,31 @@ namespace CivilIDWeb.Controllers
         [Route("/auth/getperson/callback")]
         public IActionResult PaciCallback([FromBody] PACICallbackResponse callbackResponse)
         {
-            Logger.LogDebug("called auth/getperson/callback");
-            var requestId = callbackResponse.MIDAuthSignResponse.RequestDetails.RequestID;
-            var tcs = requestTcses[requestId];
-            bool isRemoved = requestTcses.Remove(requestId, out _);
-            tcs.TrySetResult(callbackResponse);
+            try
+            {
+                Logger.LogInformation($"Request came to API /auth/getperson/callback");
+                var jsonStr = JsonSerializer.Serialize(callbackResponse);
+                Logger.LogInformation($"JSON body ==> {jsonStr}");
+                var requestId = callbackResponse.MIDAuthSignResponse.RequestDetails.RequestID;
+                Logger.LogInformation($"RequestId from PACI callback ==> {requestId}");
+                var tcs = requestTcses[requestId];
+                Logger.LogInformation($"RequestId found in the dictionary ==> {requestId}");
+                bool isRemoved = requestTcses.Remove(requestId, out _);
+                Logger.LogInformation($"RequestId removed from the dictionary ==> {requestId}");
+                tcs.TrySetResult(callbackResponse);
+                Logger.LogInformation($"Task completed for the RequestId ==> {requestId}");
 
-            // find and remove completed tasks - intermittent tasks
-            //logic starts
-            //tcs.Task.Creat
+                // find and remove completed tasks - intermittent tasks
+                //logic starts
+                //tcs.Task.Creat
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message, ex.StackTrace);
+                return BadRequest();
+            }
         }
 
         [HttpGet]
@@ -53,6 +68,8 @@ namespace CivilIDWeb.Controllers
         public async Task<IActionResult> GetPersonDetails(string civilno)
         {
             //var check = context.PacirequestLogs.ToList();
+
+            Logger.LogInformation($"Request came to API /getperson/{civilno}");
 
             var certThumbprint = configuration.GetValue<string>("certThumbprint");
             var paciServiceHostName = configuration.GetValue<string>("paciServiceHostName");
@@ -62,37 +79,50 @@ namespace CivilIDWeb.Controllers
 
             var callbackUrl = "https://" + Request.Host + "auth/getperson/callback";
 
+            Logger.LogInformation($"Request parameters from appsettings.json "
+                                  + $"\n certThumbprint: {certThumbprint}\n"
+                                  + $"\n paciServiceHostName: {paciServiceHostName}\n"
+                                  + $"\n paciServicePort: {paciServicePort}\n"
+                                  + $"\n authRequest: {JsonSerializer.Serialize(authRequest)}\n"
+                                  + $"\n callbackUrl: {callbackUrl}\n"
+                                  );
+
 #pragma warning disable SYSLIB0026 // Type or member is obsolete
             X509Certificate2 cert = new X509Certificate2();
 #pragma warning restore SYSLIB0026 // Type or member is obsolete
 
-            //X509Store x509Store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
-            //x509Store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
-            //var certificate2Collection = x509Store.Certificates.Find(X509FindType.FindByThumbprint, certThumbprint, false);
-            //if (certificate2Collection == null || certificate2Collection.Count == 0 ||
-            //certificate2Collection.Count > 1)
-            //{
-            //    throw new Exception("Certificate not found");
-            //}
-            //cert = certificate2Collection[0];
-            //var authClient = new MIDAuthServiceClient(cert, paciServiceHostName, paciServicePort);
+            X509Store x509Store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            x509Store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+            var certificate2Collection = x509Store.Certificates.Find(X509FindType.FindByThumbprint, certThumbprint, false);
+            if (certificate2Collection == null || certificate2Collection.Count == 0 ||
+            certificate2Collection.Count > 1)
+            {
+                Logger.LogError($"Certificate not found for the provided thumbprint - {certThumbprint}");
+                throw new Exception("Certificate not found");
+            }
+            cert = certificate2Collection[0];
+            var authClient = new MIDAuthServiceClient(cert, paciServiceHostName, paciServicePort);
+            Logger.LogInformation($"MIDAuthServiceClient Created");
 
-            //var resPACI = authClient.InitiateAuthRequestPN(
-            //    new AuthenticateRequest
-            //    {
-            //        ServiceProviderId = authRequest.ServiceProviderId,
-            //        ServiceDescriptionEN = authRequest.ServiceDescriptionEN,
-            //        ServiceDescriptionAR = authRequest.ServiceDescriptionAR,
-            //        AuthenticationReasonEn = authRequest.AuthenticationReasonEn,
-            //        AuthenticationReasonAr = authRequest.AuthenticationReasonAr,
-            //        RequestUserDetails = true,
-            //        SPCallbackURL = callbackUrl,
-            //        PersonCivilNo = civilno
-            //    });
+            var resPACI = authClient.InitiateAuthRequestPN(
+                new AuthenticateRequest
+                {
+                    ServiceProviderId = authRequest.ServiceProviderId,
+                    ServiceDescriptionEN = authRequest.ServiceDescriptionEN,
+                    ServiceDescriptionAR = authRequest.ServiceDescriptionAR,
+                    AuthenticationReasonEn = authRequest.AuthenticationReasonEn,
+                    AuthenticationReasonAr = authRequest.AuthenticationReasonAr,
+                    RequestUserDetails = true,
+                    SPCallbackURL = callbackUrl,
+                    PersonCivilNo = civilno
+                });
+            Logger.LogInformation($"Initiated request to mobile id server through SDK");
 
             var tcs = new TaskCompletionSource<PACICallbackResponse>();
             
-            requestTcses.TryAdd(/*resPACI.Data*/"7a80c36f-95d1-4406-889e-13adc9fecf61", tcs);
+            requestTcses.TryAdd(resPACI.Data/*"7a80c36f-95d1-4406-889e-13adc9fecf61"*/, tcs);
+            Logger.LogInformation($"Added task to DIctionary with request id - {resPACI.Data}");
+            Logger.LogInformation($"Waiting for the callback response from the mobileid server with request id - {resPACI.Data}");
 
             return Ok(await tcs.Task);
         }
